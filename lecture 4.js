9e419d1d649088678d55aea033050c2e005dcca5
FarmonovAzamat <farmonovazamat21@gmail.com>
2023-04-20 05:23:22 +0300

    first commit

---
+++ lecture 4.js
@@ -0,0 +1,10 @@
+// ---------------------ARRAY-----------------//
+//1) changing elements 
+// let a = ['eat','sleep', 'get','read', 'study'];
+// a[2]='exercise';
+// console.log(a);
+
+//2) --------------ARRAy Methods------//
+/*  pop() push() shift() unshift() toString() splice() slice() reverse() concat() join() map() filter() find() reduce()  */ 
+let a = ['eat','sleep'];
+a.push('book')
